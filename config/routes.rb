47530f5698eb7a946a7d6cf7df22b61bfda2f5e0
James Sneath <james_sneath@outlook.com>
2021-05-29 12:01:43 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :pets do
-    resources :enquiries, only [:new, :create]
+    resources :enquiries, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :enquiries, only [:index, :show, :edit, :update, :destroy]
+  resources :enquiries, only: [:index, :show, :edit, :update, :destroy]
 end
