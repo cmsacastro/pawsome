e128063a192a7b544ed5e01d71c1bea35f63aa63
Catarina Castro <cmsacastro@gmail.com>
2021-06-05 10:45:35 +0100

    added special_needs to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_183948) do
-
+ActiveRecord::Schema.define(version: 2021_06_01_194623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,7 +93,6 @@ ActiveRecord::Schema.define(version: 2021_05_29_183948) do
     t.string "first_name"
     t.string "last_name"
     t.string "company_name"
-
     t.boolean "garden", default: false
     t.boolean "other_children", default: false
     t.boolean "other_pets", default: false
@@ -104,9 +102,8 @@ ActiveRecord::Schema.define(version: 2021_05_29_183948) do
     t.string "age"
     t.text "description"
     t.string "address"
-
+    t.boolean "special_needs", default: false
     t.string "nickname"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
