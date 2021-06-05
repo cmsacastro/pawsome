8573be5368855c59625dce01b6e77b8c73a606e8
Catarina Castro <cmsacastro@gmail.com>
2021-06-05 10:13:23 +0100

    preference page

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :enquiries, only: [:index, :show, :edit, :update, :destroy]
   get 'profile', action: :show, controller: 'users'
+  get 'preference', action: :edit, controller: 'users'
 end
