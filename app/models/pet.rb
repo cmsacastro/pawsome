72d5357536a56ed25f1f8f4bcf1de9226951d1a7
Catarina Castro <cmsacastro@gmail.com>
2021-06-10 20:26:29 +0100

    some front end stuff

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -7,13 +7,14 @@ class Pet < ApplicationRecord
 
   def match_pets(user)
     matching = 0
-    points = 20
-    matching += 7 if self.breed == user.breed
-    matching += 4 if self.special_needs == user.special_needs
-    matching += 4 if self.age == user.age
-    matching += 2 if self.garden == user.garden
-    matching += 1 if self.other_pets == user.other_pets
-    matching += 1 if self.other_children == user.other_children
+    points = 45
+    matching += 10 if self.breed == user.breed
+    matching += 7 if self.sex == user.sex
+    matching += 6 if self.special_needs == user.special_needs
+    matching += 10 if self.age == user.age
+    matching += 5 if self.garden == user.garden
+    matching += 3 if self.other_pets == user.other_pets
+    matching += 3 if self.other_children == user.other_children
     matching += 1 if self.available_time == user.available_time
     matching_percentage = matching.to_f / points * 100
     matching_percentage.to_i
