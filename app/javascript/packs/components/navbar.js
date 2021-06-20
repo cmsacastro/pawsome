888bd5ca1597b854dd84c7442ede58a85c9e09b0
Catarina Castro <cmsacastro@gmail.com>
2021-06-20 17:54:01 +0100

    change color to pink, carousel, matching word navbar and avatar fixed

--- app/javascript/packs/components/navbar.js
+++ app/javascript/packs/components/navbar.js
@@ -1,11 +1,21 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-transparent');
+  const navbar2 = document.querySelector('.navbar-white-none');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= 56) {
         navbar.classList.add('navbar-white');
       } else {
         navbar.classList.remove('navbar-white');
+      }
+    });
+  }
+  if (navbar2) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 56) {
+        navbar2.classList.add('navbar-white');
+      } else {
+        navbar2.classList.remove('navbar-white');
       }
     });
   }
